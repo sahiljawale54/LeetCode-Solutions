fcb7727d33acfdc7ed139d0356470e5df1c9eeff
Sahil Jawale <20510057.dypit@dypvp.edu.in>
2022-09-18 17:17:30 +0530

    Create checkprime.cpp

---
+++ checkprime.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        int count= 0;
+        vector<bool> prime(n+1,true);
+        prime[0]= prime[1]= false;
+        
+        for(int i = 2; i < n ; i++){
+            
+            if(prime[i]){
+                count++;
+                
+                //mark ele 
+                for(int j = 2*i; j<n; j=j+i){
+                    prime[j]= false;
+                }
+            }
+        }
+       return count; 
+    }
+};
+
+//TC - O(N*LOG(LOGN)))
